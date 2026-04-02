65f91ff86e12b769abaa259e7c4f2314bd575ded
theryananding <219714321+theryananding@users.noreply.github.com>
2026-04-02 14:29:57 -0600

    Create function to get the computer's choice between rock, paper, or scissors.

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Hello world!');
+function getComputerChoice() {
+    let getRandomInt = (max) => {
+        return Math.floor(Math.random() * max);
+    }
+
+    let result;
+
+    if (getRandomInt(3) === 0) {
+        result = "rock";
+    } else if (getRandomInt(3) === 1) {
+        result = "paper";
+    } else {
+        result = "scissors";
+    }
+    
+    return result;
+}
+
+console.log(getComputerChoice()); 
